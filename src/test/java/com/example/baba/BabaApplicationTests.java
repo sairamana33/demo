c64b2cc743a8924deba511cba8e9a66494105713
sairamana33 <ramanasai033@gmail.com>
2023-09-04 09:24:26 +0530

    springbootdemoapp

---
+++ src/test/java/com/example/baba/BabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
